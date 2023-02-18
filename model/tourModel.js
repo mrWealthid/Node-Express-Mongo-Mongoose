083c5839c4cb2eaddd41023395fc01ff3b0daa11
wealth iduwe <wealthiduwe@gmail.com>
2023-02-18 20:17:45 +0100

    Update: Updated Model and created tours using model

--- model/tourModel.js
+++ model/tourModel.js
@@ -1,15 +1,50 @@
 const mongoose = require('mongoose');
 
 const tourSchema = new mongoose.Schema({
-  name: { type:String, required:[true, 'A tour must have a name'], unique: true },
-  rating: { type:Number, default:4.5 },
-  
-  price:{ type:Number, required:[true, 'A tour must have a price'] }
-})
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true,
+    trim: true,
+  },
 
+  ratingQuantity: { type: Number, default: 0 },
+  ratingAverage: { type: Number, default: 4.5 },
+  duration: { type: Number, required: [true, 'A tour must have a duration'] },
+  maxGroupSize: {
+    type: Number,
+    required: [true, 'A tour must have a group size'],
+  },
+  difficulty: {
+    type: String,
+    required: [true, 'A tour must have a difficulty level'],
+  },
+  priceDiscount: Number,
 
+  summary: {
+    type: String,
+    trim: true,
+    required: [true, 'A tour must have a description'],
+  },
 
+  price: { type: Number, required: [true, 'A tour must have a price'] },
+  description: {
+    type: String,
+    trim: true,
+  },
+  imageCover: {
+    type: String,
+    required: [true, 'A tour must have a cover image'],
+  },
 
+  images: [String],
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
 
-const Tour= mongoose.model("Tour", tourSchema)
-module.exports = Tour
+  startDates: [Date],
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
+module.exports = Tour;
