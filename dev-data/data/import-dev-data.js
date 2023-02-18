840da71ab661067c6aca383097fc84af6fe80071
wealth iduwe <wealthiduwe@gmail.com>
2023-02-18 21:02:01 +0100

    Update: Created a script that drops the db and auto upload to the db base test data

---
+++ dev-data/data/import-dev-data.js
@@ -0,0 +1,51 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const fs = require('fs');
+const Tour = require('../../model/tourModel');
+
+dotenv.config({ path: './config.env' });
+
+console.log(process.env.DATABASE);
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log('DB connection successfully'));
+// Read JSON FILE
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8')
+);
+const importData = async () => {
+  try {
+    await Tour.create(tours);
+    console.log('data successfully loaded');
+    process.exit();
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+const deleteData = async () => {
+  try {
+    await Tour.deleteMany();
+    console.log('data successfully deleted');
+    process.exit();
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+if (process.argv[2] === '--import') {
+  importData();
+} else if (process.argv[2] === '--delete') {
+  deleteData();
+}
+
+console.log(process.argv);
