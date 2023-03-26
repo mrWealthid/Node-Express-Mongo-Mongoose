31d0fcc08e142b74f8990b6c325afb0a5008ef33
Wealth <wealthiduwe@gmail.com>
2023-03-26 15:53:37 +0100

    Update: script was updated to point to a different tour resource

--- dev-data/data/import-dev-data.js
+++ dev-data/data/import-dev-data.js
@@ -19,9 +19,7 @@ mongoose
   })
   .then(() => console.log('DB connection successfully'));
 // Read JSON FILE
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8')
-);
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours.json`, 'utf-8'));
 const importData = async () => {
   try {
     await Tour.create(tours);
