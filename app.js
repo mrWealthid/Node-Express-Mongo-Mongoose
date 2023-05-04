13ca0e817bf04465af364837e856dd3d28589c06
Wealth <wealthiduwe@gmail.com>
2023-05-04 16:04:07 +0100

    fix: fixed cors to address cross origin issues

--- app.js
+++ app.js
@@ -6,11 +6,13 @@ const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
 const hpp = require('hpp');
+const cors = require('cors');
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const reviewRouter = require('./routes/reviewRoutes');
+const bookingRouter = require('./routes/bookingRoutes');
 
 const filepath = path.join(process.cwd(), 'public');
 
@@ -22,6 +24,8 @@ console.log(process.env.NODE_ENV);
 
 //SET Security HTTP Headers
 app.use(helmet());
+
+app.use(cors());
 
 //Development Logging
 if (process.env.NODE_ENV === 'development') {
@@ -76,6 +80,7 @@ app.use((req, res, next) => {
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter);
+app.use('/api/v1/bookings', bookingRouter);
 
 app.all('*', (req, res, next) => {
   next(new AppError(`can't find ${req.originalUrl} on this server!`, 404));
