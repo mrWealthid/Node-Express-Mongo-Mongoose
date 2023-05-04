8efc07c52546fc966de386c9f1700aa434fcc563
Wealth <wealthiduwe@gmail.com>
2023-05-04 16:03:01 +0100

    Add: Integrated Stripe payment gateway for checkouts and bookings

---
+++ controllers/bookingController.js
@@ -0,0 +1,55 @@
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+const Tour = require('../model/tourModel');
+const Booking = require('../model/bookingModel');
+const catchAsync = require('../utils/catchAsync');
+const AppError = require('../utils/appError');
+const factory = require('./handlerFactory');
+
+exports.getCheckoutSession = catchAsync(async (req, res, next) => {
+  //Get currently booked tour
+  const tour = await Tour.findById(req.params.tourID);
+
+  //Get Checkout Session
+
+  const session = await stripe.checkout.sessions.create({
+    payment_method_types: ['card'],
+    success_url: `${req.protocol}://${req.get('host')}/?tour=${
+      req.params.tourId
+    }&user=${req.user.id}&price=${tour.price}`,
+    cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`,
+    customer_email: req.user.email,
+    client_reference_id: req.params.tourID,
+    mode: 'payment',
+    line_items: [
+      {
+        price_data: {
+          currency: 'usd',
+          unit_amount: tour.price * 100,
+          product_data: {
+            name: `${tour.name} Tour`,
+            description: tour.summary,
+            images: [`https://www.natours.dev/img/tours/${tour.imageCover}`],
+          },
+        },
+        quantity: 1,
+      },
+    ],
+  });
+
+  res.status(200).json({
+    status: 'success',
+    session,
+  });
+  //Create session as response
+});
+
+exports.createBookingCheckout = catchAsync(async (req, res, next) => {
+  //THIS IS ONLY TEMPORARY BECAUSE EVERYONE CAN MAKE BOOKINGS WITHOUT PAYING
+
+  const { tour, user, price } = req.query;
+
+  if (!tour && !user && !price) return next();
+  await Booking.create({ tour, user, price });
+
+  res.redirect(req.originalUrl.split('?')[0]);
+});
