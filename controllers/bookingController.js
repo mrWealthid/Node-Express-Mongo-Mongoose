21dfb131e260db17ae8360eefd1f615fd1cab304
Wealth <wealthiduwe@gmail.com>
2023-05-10 16:56:54 +0100

    Updates

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -53,3 +53,9 @@ exports.createBookingCheckout = catchAsync(async (req, res, next) => {
 
   res.redirect(req.originalUrl.split('?')[0]);
 });
+
+exports.createBooking = factory.createOne(Booking);
+exports.getBooking = factory.getOne(Booking);
+exports.getAllBooking = factory.getAll(Booking);
+exports.updateBooking = factory.updateOne(Booking);
+exports.deleteBooking = factory.deleteOne(Booking);
